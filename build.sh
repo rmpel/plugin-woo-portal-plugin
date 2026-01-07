d0d4b92af95f64bcc11e7a277007d2119e443d23
Remon Pel <remon@acato.nl>
2026-01-07 11:48:56 +0100

    Fix build?

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 [ -f ~/.bashrc ] && source ~/.bashrc
 
-nvm use 20 || echo "could not select node version 20."
+nvm use 20 || nvm install 20 || echo "could not select node version 20."
 
 npm install
 npm run build
